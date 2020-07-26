63d2b6201a99482c39bf8019e2ca137e61a6fb19
Raphael Barreto <tnt.raphael@gmail.com>
2020-07-26 07:30:43 -0300

    The useQuery Hook

--- src/components/People.js
+++ src/components/People.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Person from './Person';
+
+const fetchPlanets = async () => {
+  const response = await fetch('http://swapi.dev/api/people');
+
+  return response.json();
+};
 
 const People = () => {
+  const { data, status } = useQuery('people', fetchPlanets);
+
   return (
     <div>
       <h2>People</h2>
+
+      {status === 'loading' && <div>Loading data...</div>}
+
+      {status === 'error' && <div>Error fetching data</div>}
+
+      {status === 'success' && (
+        <div>
+          {data.results.map(person => (
+            <Person key={person.name} person={person} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Planet from './Planet';
+
+const fetchPlanets = async () => {
+  const response = await fetch('http://swapi.dev/api/planets');
+
+  return response.json();
+};
 
 const Planets = () => {
+  const { data, status } = useQuery('planets', fetchPlanets);
+
   return (
     <div>
       <h2>Planets</h2>
+
+      {status === 'loading' && <div>Loading data...</div>}
+
+      {status === 'error' && <div>Error fetching data</div>}
+
+      {status === 'success' && (
+        <div>
+          {data.results.map(planet => (
+            <Planet key={planet.name} planet={planet} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };

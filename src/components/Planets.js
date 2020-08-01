eda1a313c1cc324f58258e91ed1dada122b924b9
Raphael Barreto <tnt.raphael@gmail.com>
2020-07-31 22:06:53 -0300

    React Query Dev Tools

--- src/components/Planets.js
+++ src/components/Planets.js
@@ -9,7 +9,10 @@ const fetchPlanets = async () => {
 };
 
 const Planets = () => {
-  const { data, status } = useQuery('planets', fetchPlanets);
+  const { data, status } = useQuery('planets', fetchPlanets, {
+    staleTime: 2000,
+    // cacheTime: 10,
+  });
 
   return (
     <div>
